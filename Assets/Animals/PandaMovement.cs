041bc43894fccc2f0ca1cd1757c2cdafdb99a93a
shujian97 <guanshujian97@gmail.com>
2022-02-15 17:44:03 +0100

    a

---
+++ Assets/Animals/PandaMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PandaMovement : MonoBehaviour
+{
+
+    public float waitTime = 5.0f;
+    private float timer = 0.0f;
+    Vector3 backRotation = new Vector3(0, 0, 0);
+    Vector3 frontRotation = new Vector3(0, 180, 0);
+    private int counter = 0;
+    //public Transform explosion = (GameObject)Resouces.Load("Prefabs/Explosion", typeof(GameObject));
+
+    void OnCollisionEnter()
+    {
+        //GameObject exploder = ((Transform)Instantiate(explosion, this.transform.position,
+        //    this.transform.rotation)).gameObject;
+       // Destroy(exploder, 2.0f);
+        Destroy(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+        //transform.eulerAngles = newRotation;
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > waitTime && counter == 0)
+        {
+            transform.eulerAngles = backRotation;
+            timer = timer - waitTime;
+            counter = 1;
+            Debug.Log("Panda Rotated Back");
+        }
+
+        if(timer > waitTime && counter == 1)
+        {
+            transform.eulerAngles = frontRotation;
+            timer = timer - waitTime;
+            counter = 0;
+            Debug.Log("Panda Rotated Front");
+
+        }
+    }
+    
+    
+}
